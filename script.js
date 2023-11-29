53ae836045427b02de8fdf58366d2d0602284007
Katasravani <105917453+Katasravani@users.noreply.github.com>
2023-11-29 16:04:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+// script.js
+
+function removeColor() {
+    // Get the dropdown list reference by its ID
+    let colorSelect = document.getElementById('colorSelect');
+
+    // Remove the selected option
+    colorSelect.remove(colorSelect.selectedIndex);
+}
